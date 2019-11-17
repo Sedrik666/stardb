9b8e406d2268cf35a2e1502759a28e84431d5f38
Sedrik666 <fedvetal@mail.ru>
2019-11-17 03:05:34 +0200

    feat(fetch): processing errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const getResource = async (url) => {
     const res = await fetch(url);
-        const body = await res.json();
+    if(!res.ok){
+        throw new Error(`Could not fetch ${url}` +
+            ` received ${res.status}`)
+    }
+    const body = await res.json();
     return body;
 };
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/121212/')
     .then((body) => {
           console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err);
     });
   
