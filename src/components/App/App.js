b6e4fd6c153ac18a850a9326baa8c3587dcb340a
Sedrik666 <fedvetal@mail.ru>
2019-11-26 23:15:15 +0200

    feat(ItemList): use componentDidCatch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,19 @@
 import React, {Component} from 'react';
 import Header from '../Header';
-import ItemList from '../ItemList';
 import RandomPlanet from '../RandomPlanet';
-import PersonDetails from '../PersonDetails';
 
 import './App.css'
+import PeoplePage from "../PeoplePage";
 
 export default class App extends Component {
-    state = {
-        selectedPerson: 5,
-    };
-
-    onPersonSelected = (id) => {
-        this.setState({selectedPerson: id})
-    };
     render(){
         return(
             <div>
                 <Header/>
                 <RandomPlanet />
-                <div className='row mb2'>
-                    <div className="col-md-6">
-                        <ItemList onItemSelected = {this.onPersonSelected}/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
-                </div>
+                <PeoplePage/>
+                <PeoplePage/>
+                <PeoplePage/>
             </div>
         );
     }
