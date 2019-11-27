d79a38a54e10849e44e1706a1a57e58dcbb0f482
Sedrik666 <fedvetal@mail.ru>
2019-11-28 01:43:49 +0200

    refactor(App): remove unnecessary and replace someone components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,8 @@ import RandomPlanet from '../RandomPlanet';
 
 import './App.css'
 import PeoplePage from "../PeoplePage";
-import ItemList from "../ItemList";
-import PersonDetails from "../PersonDetails";
+import ErrorButton from "../ErrorButton";
+import ErrorBoundary from "../ErrorBoundary";
 import SwapiService from "../../services/SwapiService";
 
 export default class App extends Component {
@@ -16,35 +16,35 @@ export default class App extends Component {
         hasError: false,
     };
 
+    toggleRandomPlanet = () => {
+        this.setState((state) => {
+            return {
+                showRandomPlanet: !state.showRandomPlanet
+            }
+        });
+    };
+
     render(){
+        const planet = this.state.showRandomPlanet ?
+            <RandomPlanet/> :
+            null;
+
         return(
-            <div>
-                <Header/>
-                <RandomPlanet />
-                <PeoplePage/>
-                {/*<div className='row mb2'>
-                    <div className="col-md-6">
-                        <ItemList onItemSelected = {this.onPersonSelected}
-                                  getData = { this.swapiService.getAllPlanets}
-                                  renderItem = {(item) => (<span>{item.name}<button>!</button></span>)}
-                        />
+            <ErrorBoundary>
+                <div className="stardb-app">
+                    <Header/>
+                    { planet }
+                    <div className="row mb2 button-row">
+                        <button
+                            className="toggle-planet btn btn-warning btn-lg"
+                            onClick={this.toggleRandomPlanet}>
+                            Toggle Random Planet
+                        </button>
+                        <ErrorButton />
                     </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
+                    <PeoplePage/>
                 </div>
-                <div className='row mb2'>
-                    <div className="col-md-6">
-                        <ItemList onItemSelected = {this.onPersonSelected}
-                                  getData = { this.swapiService.getAllStarships}
-                                  renderItem = {(item) => item.name}
-                        />
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
-                </div>*/}
-            </div>
+            </ErrorBoundary>
         );
     }
 };
--- src/components/PeoplePage/PeoplePage.js
+++ src/components/PeoplePage/PeoplePage.js
@@ -3,38 +3,11 @@ import React, { Component } from 'react';
 import './PeoplePage.css';
 import ItemList from "../ItemList";
 import PersonDetails from "../PersonDetails";
-import ErrorIndicator from "../ErrorIndicator";
+import ErrorBoundary from "../ErrorBoundary";
 import SwapiService from "../../services/SwapiService";
+import Row from "../Row";
 
-const Row = ({left, right}) => {
-    return(
-        <div className='row mb2'>
-            <div className="col-md-6">
-                {left}
-            </div>
-            <div className="col-md-6">
-                {right}
-            </div>
-        </div>
-    );
-};
 
-class ErrorBoundary extends Component{
-    state = {
-        hasError: false,
-    };
-
-    componentDidCatch() {
-        this.setState({hasError: true})
-    }
-
-    render(){
-        if(this.state.hasError){
-            return <ErrorIndicator/>
-        }
-        return this.props.children
-    }
-}
 
 export default class PeoplePage extends Component{
     swapiService = new SwapiService();
