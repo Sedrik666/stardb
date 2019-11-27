ef0b96057d2a5ae2a9304e373875f4dab28a5254
Sedrik666 <fedvetal@mail.ru>
2019-11-28 00:39:36 +0200

    feat(pattern): Using functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,44 @@ import RandomPlanet from '../RandomPlanet';
 
 import './App.css'
 import PeoplePage from "../PeoplePage";
+import ItemList from "../ItemList";
+import PersonDetails from "../PersonDetails";
+import SwapiService from "../../services/SwapiService";
 
 export default class App extends Component {
+    swapiService = new SwapiService();
+
+    state={
+        selectedPerson: 3,
+        hasError: false,
+    };
+
     render(){
         return(
             <div>
                 <Header/>
                 <RandomPlanet />
                 <PeoplePage/>
-                <PeoplePage/>
-                <PeoplePage/>
+                <div className='row mb2'>
+                    <div className="col-md-6">
+                        <ItemList onItemSelected = {this.onPersonSelected}
+                                  getData = { this.swapiService.getAllPlanets}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson}/>
+                    </div>
+                </div>
+                <div className='row mb2'>
+                    <div className="col-md-6">
+                        <ItemList onItemSelected = {this.onPersonSelected}
+                                  getData = { this.swapiService.getAllStarships}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson}/>
+                    </div>
+                </div>
             </div>
         );
     }
